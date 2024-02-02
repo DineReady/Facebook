ac80d908f2fbfabb110ddf6d10f73543d30884c9
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-02-02 08:54:21 +0100

    Add Prettier configuration file and update Tailwind CSS imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-	return <div className='App'>client</div>;
+    return <div className="App">client</div>;
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement,
 );
 root.render(<App />);
 
