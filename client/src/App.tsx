ea185924cd70d5d070f01e368e4d64000b55495a
Gorbkos <gniewkoscielak@gmail.com>
2024-02-02 14:39:46 +0100

    Add order status and image display to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
+import PreparationImage from "./images/food.webp";
+import Done from "./images/correct.png";
+import Logo from "./images/logo.svg";
+import { OrderStatus } from "./types/types";
 
 function App() {
-    return <div className="text-red-500">client</div>;
+    const [orderStatus, setOrderStatus] = useState(OrderStatus.PREPARATION);
+    const [image, setImage] = useState(PreparationImage);
+
+    useEffect(() => {
+        setImage(
+            orderStatus === OrderStatus.PREPARATION ? PreparationImage : Done,
+        );
+    }, [orderStatus]);
+
+    return (
+        <div className="waiting-screen text-5xl flex justify-center items-center flex-col">
+            <img className="logo" src={Logo} alt="" />
+            <img className="orderImage" src={image} alt="" />
+            <h1>{orderStatus}</h1>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/types/types.ts
@@ -0,0 +1,4 @@
+export enum OrderStatus {
+    PREPARATION = "Your order is being prepared. Please wait...",
+    READY = "Your order is ready to be served.",
+}
