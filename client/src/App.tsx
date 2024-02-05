dc55c68549db707b12d0e24f6810d9f797ae09e7
Gorbkos <gniewkoscielak@gmail.com>
2024-02-05 19:23:45 +0100

    Update Lottie animation in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,8 +27,13 @@ export default function App(): JSX.Element {
     return (
         <main className="main-section">
             <img className="logo" src={Logo} alt="logo" />
-            <div>
-                <Lottie options={defaultOptions} height={500} width={500} />
+            <div style={{ pointerEvents: "none" }}>
+                <Lottie
+                    options={defaultOptions}
+                    height={500}
+                    width={500}
+                    isStopped={false}
+                />
             </div>
             <h1 className="response-text">{orderStatus}</h1>
         </main>
