bee94bb8b0e90ec2f57d3fb6a25bb93642a2a6b5
Gorbkos <gniewkoscielak@gmail.com>
2024-02-05 18:43:08 +0100

    added gif

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./globals.css";
 import { OrderStatus } from "./types";
 import { Done, Logo, PreparationImage } from "./assets";
+import Lottie from "react-lottie";
+import animationData from "./assets/food.json";
 
 export default function App(): JSX.Element {
     const [orderStatus, setOrderStatus] = useState(OrderStatus.PREPARATION);
@@ -13,10 +15,20 @@ export default function App(): JSX.Element {
         );
     }, [orderStatus]);
 
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        renderer: "svg",
+    };
+
     return (
         <main className="main-section">
             <img className="logo" src={Logo} alt="logo" />
-            <img src={image} alt="order_img" />
+            {/* <img src={image} alt="order_img" /> */}
+            <div>
+                <Lottie options={defaultOptions} height={500} width={500} />
+            </div>
             <h1 className="response-text">{orderStatus}</h1>
         </main>
     );
