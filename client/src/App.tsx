7eccfadb1dbde99b052b1aa44462d30a056bdde0
Gorbkos <gniewkoscielak@gmail.com>
2024-02-07 14:07:52 +0100

    Update App.tsx styling and layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,17 +24,17 @@ export default function App(): JSX.Element {
     };
 
     return (
-        <main className="main-section">
-            <img className="logo" src={Logo} alt="logo" />
+        <main className="" style={{ overflowX: "hidden" }}>
+            <img className="logo p-6" src={Logo} alt="logo" />
             <div style={{ pointerEvents: "none" }}>
                 <Lottie
                     options={defaultOptions}
-                    height={500}
-                    width={500}
+                    height={400}
+                    width={400}
                     isStopped={false}
                 />
             </div>
-            <h1 className="response-text">{orderStatus}</h1>
+            <h1 className="response-text p-6 mt-10 text-4xl">{orderStatus}</h1>
         </main>
     );
 }
