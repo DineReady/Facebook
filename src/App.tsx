cf6ddadfc3bb3cf997f221ca1a6559a15bbb5049
Gorbkos <gniewkoscielak@gmail.com>
2024-02-22 08:38:42 +0100

    Refactor App component and update translation.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,15 @@
-// /* eslint-disable indent */
-// import React, { useState, useEffect } from "react";
-// import "./globals.css";
-// import { OrderStatus } from "./types";
-// import Lottie from "lottie-react";
-// import { DoneAnimation, PreparingAnimation } from "./assets";
-// import { Header, LottieAnimation, Response } from "./components";
-// import { NavigateFunction, useNavigate, useParams } from "react-router-dom";
-// import axios from "axios";
-// import { useTranslation } from "react-i18next";
-// import ChangeLanguageButton from "./components/ChangeLangBtn";
-
-// interface Options {
-//     animationData: any;
-// }
-
-// export default function App(): JSX.Element {
-//     const [orderStatus, setOrderStatus] = useState<OrderStatus>(
-//         OrderStatus.ERROR,
-//     );
-//     const [image, setImage] =
-//         useState<Options["animationData"]>(PreparingAnimation);
-//     const { id } = useParams();
-//     const navigate: NavigateFunction = useNavigate();
-
-//     useEffect(() => {
-//         async function getOrderDetails() {
-//             try {
-//                 const order = await axios.get(
-//                     `http://localhost:8080/orders/${id}`,
-//                 );
-//                 const orderStatus: string = order.data.status;
-//                 console.log(order.data);
-//                 setOrderStatus(orderStatus as OrderStatus);
-//                 setImage(
-//                     orderStatus === "DONE" ? DoneAnimation : PreparingAnimation,
-//                 );
-//             } catch (error) {
-//                 console.error(error);
-//             }
-//         }
-//         getOrderDetails();
-//     }, [id]);
-//     const { t } = useTranslation();
-
-//     return (
-//         <main style={{ overflowX: "hidden" }}>
-//             <Header />
-//             <LottieAnimation animationData={image} />
-//             <Response orderStatus={orderStatus} />
-
-//             <h1 className="">asadas</h1>
-//             <h1>{t("myPageTitle")}</h1>
-//             <ChangeLanguageButton />
-//             <h2>{t("myHeader")}</h2>
-//             <p>{t("myDescription")}</p>
-//         </main>
-//     );
-// }
-
 /* eslint-disable indent */
 import React, { useState, useEffect } from "react";
 import "./globals.css";
 import { OrderStatus } from "./types";
-import { Options } from "react-lottie";
 import { DoneAnimation, PreparingAnimation } from "./assets";
 import { Header, LottieAnimation, Response } from "./components";
 import { NavigateFunction, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+
+interface Options {
+    animationData: any;
+}
 
 export default function App(): JSX.Element {
     const [orderStatus, setOrderStatus] = useState<OrderStatus>(
@@ -77,35 +20,24 @@ export default function App(): JSX.Element {
     const { id } = useParams();
     const navigate: NavigateFunction = useNavigate();
 
-    async function getOrderDetails() {
-        try {
-            const order = await axios.get(`http://localhost:8080/orders/${id}`);
-            const orderStatus: string = order.data.status;
-            console.log(order.data);
-            setOrderStatus(
-                orderStatus === "pending"
-                    ? OrderStatus.PREPARATION
-                    : OrderStatus.READY,
-            );
-            setImage(
-                orderStatus === OrderStatus.READY
-                    ? DoneAnimation
-                    : PreparingAnimation,
-            );
-        } catch (error) {
-            console.error(error);
-            setOrderStatus(OrderStatus.ERROR);
-        }
-    }
-
-    // todo: App context to handle the order status error for <Error />
     useEffect(() => {
-        if (id?.length !== 36 || !id) {
-            // setImage() TODO @Gniewkoss: Add ERROR Lottie animation
-            setOrderStatus(OrderStatus.ERROR);
-            navigate("/");
-        } else getOrderDetails();
-    }, [id, orderStatus, navigate]);
+        async function getOrderDetails() {
+            try {
+                const order = await axios.get(
+                    `http://localhost:8080/orders/${id}`,
+                );
+                const orderStatus: string = order.data.status;
+                console.log(order.data);
+                setOrderStatus(orderStatus as OrderStatus);
+                setImage(
+                    orderStatus === "DONE" ? DoneAnimation : PreparingAnimation,
+                );
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getOrderDetails();
+    }, [id]);
 
     return (
         <main style={{ overflowX: "hidden" }}>
